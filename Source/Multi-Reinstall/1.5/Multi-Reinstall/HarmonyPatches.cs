60962bdb2c18e0f2438347dcc08246e8ceedd98d
OELS <oelsart@gmail.com>
2025-06-23 00:10:24 +0900

    fieldを取得しつつ条件緩和

--- Source/Multi-Reinstall/1.5/Multi-Reinstall/HarmonyPatches.cs
+++ Source/Multi-Reinstall/1.5/Multi-Reinstall/HarmonyPatches.cs
@@ -97,6 +97,8 @@ namespace MultiReinstall
         {
             return AccessTools.FindIncludingInnerTypes(AccessTools.TypeByName("SmartDeconstruct.SmartDeconstructMod"), t =>
             {
+                var fields = t.GetDeclaredFields();
+                if ((field = fields.FirstOrDefault(f => f.FieldType == typeof(JobDriver) && f.Name == "__instance")) == null) return null;
                 return t.GetDeclaredMethods().FirstOrDefault(m => m.Name.Contains("<CheckForRoofsBeforeJob>b__0"));
             });
         }
